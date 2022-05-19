dcb38d4ab2f1565f264da642c3532f03c1366656
Albert Mwasisoba <almwassy@gmail.com>
2022-05-19 10:56:27 +0300

    connected routes to the server

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,12 @@ connection.once('open', () => {
 app.use(cors())
 app.use(express.json())
 
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
