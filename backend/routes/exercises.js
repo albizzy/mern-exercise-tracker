188f3e2f38d32e54b896d2b0e24378d1f50de3d1
Albert Mwasisoba <almwassy@gmail.com>
2022-05-19 23:08:35 +0300

    fixed the exercise routes file

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -23,6 +23,19 @@ router.route('/add').post((req, res) => {
     newExercise.save()
         .then(() => res.json('Exercise added'))
         .catch(err => res.status(400).json('Error: ' + err))
-})
+});
+
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercises => res.json(exercises))
+        .catch(err => res.status(400).json('Error: ' + err))
+});
+
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise was deleted'))
+        .catch(err => res.status(400).json('Error: ' + err))
+});
+
 
 module.exports = router;
